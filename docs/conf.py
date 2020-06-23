4a26467005ad98221979fb8f52c68d349cab31a9
authoress <geekwriter@yandex.ru>
2020-06-23 16:47:07 +0300

    conf updated, custom css added

--- docs/conf.py
+++ docs/conf.py
@@ -58,13 +58,19 @@ master_doc = 'index'
 html_theme = 'alabaster'
 
 html_theme_options = {
-	'fixed_sidebar': False
+	'description': 'Документация Beautiful Soup на GeekWriter.ru',
+	'github_user': 'authoress',
+	'github_repo': 'bs4ru'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
 
 html_show_sourcelink = False
 
